562493030f24d19b88ef9a52016d7edddc5482b7
Semerbl4 <69350287+Semerbl4@users.noreply.github.com>
2021-04-05 01:45:08 +0300

    Deletes actions.ts

--- src/redux/actions.ts
+++
@@ -1,6 +0,0 @@
-import { ISetSelectedElement, IElement } from './types';
-
-export const setSelectedElement = (element: IElement): ISetSelectedElement => ({
-  type: 'SET_SELECTED_ELEMENT',
-  payload: element,
-});
